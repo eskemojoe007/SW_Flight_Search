dc79f6c553ce9dd62a47795a4914b8ade31c8ecb
eskemojoe007 <David.Folkner@gmail.com>
2018-02-06 18:35:06 -0500

    Commenting and fixing.

--- get_flight_details.py
+++ get_flight_details.py
@@ -11,16 +11,24 @@ from matplotlib import pyplot as plt
 from matplotlib import dates
 import seaborn as sns
 from matplotlib.dates import HOURLY, DateFormatter, rrulewrapper, RRuleLocator,AutoDateFormatter, HourLocator
-import time
+# import time
 import requests
 import os
 import codecs
 sns.set()
 import pytz
+import logging
+# end%%
+
+# %% MAIN
+def main():
+    pass
+
 # end%%
 
 # %%
 def get_airport_codes(request_obj=None,url='https://www.southwest.com/flight/search-flight.html'):
+    logging.debug('Getting available airport codes from SW website')
     if request_obj is None:
         request_obj = requests.get(url)
         # request_obj = urllib3.urlopen(url)
@@ -39,12 +47,17 @@ def get_airport_codes(request_obj=None,url='https://www.southwest.com/flight/sea
 def get_airports_dat(fn='airports.dat'):
 
     if not os.path.isfile(fn):
+        logging.info('Could not find the %s file...downloading from github and saving'%(fn))
         response = requests.get('https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat')
         with codecs.open(fn,'w',encoding=response.encoding) as f:
             f.write(response.text)
+    else:
+        logging.debug('Found %s...'%(fn))
 
 def get_airport_df(fn='airports.dat'):
     get_airports_dat(fn=fn)
+
+    logging.debug('Converting %s to a dataframe of useful information'%(fn))
     df = pd.read_csv(fn,index_col=0,
         names=['Airport ID',
         'Name',
@@ -69,10 +82,13 @@ def check_codes(codes,available_codes=None):
     if '__iter__' in dir(codes):
         for code in codes:
             if not code.upper() in available_codes:
-                raise ValueError('Entered code %s is not valid'%code)
+                logging.error('You specified and airport code that was invalid: %s'%code)
+                raise ValueError
     else:
         if not codes.upper() in available_codes:
-            raise ValueError('Entered airport code "%s" is not valid'%codes)
+            logging.error('You specified and airport code that was invalid: %s'%code)
+            raise ValueError
+            # raise ValueError('Entered airport code "%s" is not valid'%codes)
 # end%%
 
 # %%
@@ -129,18 +145,18 @@ def strfdelta(tdelta, fmt='{D:02}d {H:02}h {M:02}m {S:02}s', inputtype='timedelt
 # end%%
 
 # %%
-def get_driver(method='Firefox'):
-    if method.lower() == 'firefox':
-            return webdriver.Firefox()
-    else:
-        raise ValueError('Invalid entry for get_driver method')
+# def get_driver(method='Firefox'):
+#     if method.lower() == 'firefox':
+#             return webdriver.Firefox()
+#     else:
+#         raise ValueError('Invalid entry for get_driver method')
 def enter_single_search(origin,destination,outbound_date_str,return_date_str,
     method='dollars',driver=None,
     url='https://www.southwest.com/flight/search-flight.html?',
     available_codes=None, wait_time=.2):
 
     if not driver is None:
-        print('Warning...you specified a driver...we no longer use selenium')
+        logging.warning('you specified a driver...we no longer use selenium, Driver should always be None...continueing')
 
     payload = {
         'twoWayTrip': True,
@@ -158,64 +174,8 @@ def enter_single_search(origin,destination,outbound_date_str,return_date_str,
 
     r =requests.post(url,params=payload)
 
+    logging.info('Got payload back from SW')
     return r
-
-
-# def enter_single_search(origin,destination,outbound_date_str,return_date_str,
-#     method='dollars',driver=None,
-#     url='https://www.southwest.com/flight/search-flight.html',
-#     available_codes=None, wait_time=.2):
-#
-#     if available_codes is None:
-#         available_codes = get_airport_codes(url=url)
-#     # Check the codes to make sure they are valid
-#     check_codes([origin,destination],available_codes=available_codes.keys())
-#
-#     #check date format - Not doing for now...should use datetime to make it right
-#
-#     #check driver
-#     if driver is None:
-#         driver = get_driver()
-#
-#
-#     #Perform the action
-#     driver.get(url)
-#     departure = driver.find_element_by_id('originAirport_displayed')
-#     departure.clear()
-#     arrival = driver.find_element_by_id('destinationAirport_displayed')
-#     arrival.clear()
-#     outbound_date = driver.find_element_by_id('outboundDate')
-#     outbound_date.clear()
-#     return_date = driver.find_element_by_id('returnDate')
-#     return_date.clear()
-#
-#     time.sleep(wait_time)
-#
-#
-#     departure.send_keys(available_codes[origin.upper()])
-#     # departure.send_keys(origin.upper())
-#     # departure.send_keys(Keys.RETURN)
-#     time.sleep(wait_time)
-#     arrival.send_keys(available_codes[destination.upper()])
-#     # arrival.send_keys(destination.upper())
-#     # arrival.send_keys(Keys.RETURN)
-#     time.sleep(wait_time)
-#     outbound_date.clear()
-#     outbound_date.send_keys(outbound_date_str)
-#     time.sleep(wait_time)
-#
-#     # outbound_date.send_keys(Keys.RETURN)
-#     return_date.clear()
-#     return_date.send_keys(return_date_str)
-#     time.sleep(wait_time)
-#     # return_date.send_keys(Keys.RETURN)
-#     time.sleep(wait_time)
-#     if method.lower() == 'dollars':
-#         radio = driver.find_element_by_id('dollars')
-#         radio.click()
-#     else:
-#         raise ValueError('Invalid search method: %s'%method)
-#     driver.find_element_by_id('submitButton').click()
 
 def get_results_soup(driver):
     return BeautifulSoup(driver.page_source,'html.parser')
@@ -224,6 +184,13 @@ def get_results_soup_request(r):
     return BeautifulSoup(r.content,'html.parser')
 
 def get_1way_df(soup,date_str,origin,destination,direction):
+    # Here...we read the soup object and extract all the useful data
+
+    # Soup - Soup object (probably created from get_results_soup_request)
+    # date_str - date of the search...its hard to get the results date...so you have to pass the searched dates
+    # origin - Searched origin str
+    # destination - 
+
     if direction.lower().strip() == 'outbound':
         table_id = 'faresOutbound'
         re_str = 'outbound_flightRow_.*'
@@ -269,6 +236,8 @@ def get_1way_df(soup,date_str,origin,destination,direction):
 gmaps = googlemaps.Client(key='AIzaSyD5342S4ws31btGQCSptFBt_knn8u683do')
 
 def get_times(origin, destination, arrival_time,traffic_model='best_guess'):
+    #Origin and Destination can either be a google searchable string or
+    # a tuple of lat_long coordinates.  Anything else could cause an error
     origin = get_lat_long_string(origin)
     destination = get_lat_long_string(destination)
 
@@ -336,12 +305,12 @@ def get_lat_long_string(lat_long_tup):
 # end%%
 
 # %% Get and sort agony
-def add_timezones(df,location_key,time_key):
-    for city in df[location_key].unique():
-        tz_code = get_timezone_code(city)
-        df.loc[df[location_key] == city, time_key] = pd.Index(df[time_key].loc[df[location_key] == city].values).tz_localize(tz_code)
-
-    df[time_key] = pd.to_datetime(df[time_key])
+# def add_timezones(df,location_key,time_key):
+#     for city in df[location_key].unique():
+#         tz_code = get_timezone_code(city)
+#         df.loc[df[location_key] == city, time_key] = pd.Index(df[time_key].loc[df[location_key] == city].values).tz_localize(tz_code)
+#
+#     df[time_key] = pd.to_datetime(df[time_key])
 
 def add_drivetimes(df,location_key,time_key,name_key,offset='2 hours',start='21 Jones Ave, Greenville, SC'):
     df[name_key] = df.apply(lambda x: get_times(start,x[location_key],(x[time_key] - pd.to_timedelta(offset)).to_pydatetime()),axis=1)
@@ -358,41 +327,41 @@ def get_sort_agony(df):
 # end%%
 
 # %% Do it allows
-def do_all_single_flight(origin,destination,outbound_date_str,return_date_str):
-    available_dict = get_airport_codes()
-    # d = get_driver()
-    # enter_single_search(origin,destination,outbound_date_str,return_date_str,driver=d,available_codes=available_dict)
-    r = enter_single_search(origin,destination,outbound_date_str,return_date_str,available_codes=available_dict)
-    soup = get_results_soup_request(r)
-    df_outbound = get_1way_df(soup,outbound_date_str,origin,destination,'outbound')
-    df_return = get_1way_df(soup,return_date_str,destination,origin,'return')
-    df = pd.c
-    add_timezones(df,'origin','depart_time')
-    add_timezones(df,'destination','arrive_time')
-    add_drivetimes(df,'origin','depart_time','depart_drive_time')
-    return get_sort_agony(df)
+# def do_all_single_flight(origin,destination,outbound_date_str,return_date_str):
+#     available_dict = get_airport_codes()
+#     # d = get_driver()
+#     # enter_single_search(origin,destination,outbound_date_str,return_date_str,driver=d,available_codes=available_dict)
+#     r = enter_single_search(origin,destination,outbound_date_str,return_date_str,available_codes=available_dict)
+#     soup = get_results_soup_request(r)
+#     df_outbound = get_1way_df(soup,outbound_date_str,origin,destination,'outbound')
+#     df_return = get_1way_df(soup,return_date_str,destination,origin,'return')
+#     df = pd.c
+#     add_timezones(df,'origin','depart_time')
+#     add_timezones(df,'destination','arrive_time')
+#     add_drivetimes(df,'origin','depart_time','depart_drive_time')
+#     return get_sort_agony(df)
 # end%%
 
 # %% Do it all multiple
-def do_all_multiple_flight(origins,destinations,outbound_date_strs,return_date_strs):
-    iters = get_iter_items(origins,destinations,outbound_date_strs,return_date_strs)
-
-    available_dict = get_airport_codes()
-    dfs = []
-    for i in iters:
-        origin = i[0]
-        destination = i[1]
-        outbound_date_str = i[2]
-        return_date_str = i[3]
-        r = enter_single_search(origin,destination,outbound_date_str,return_date_str,available_codes=available_dict)
-        soup = get_results_soup_request(r)
-        df_temp = get_1way_df(soup,outbound_date_str,origin,destination,'outbound')
-        add_timezones(df_temp,'origin','depart_time')
-        add_timezones(df_temp,'destination','arrive_time')
-        add_drivetimes(df_temp,'origin','depart_time','depart_drive_time')
-
-        dfs.append(df_temp)
-    return dfs
+# def do_all_multiple_flight(origins,destinations,outbound_date_strs,return_date_strs):
+#     iters = get_iter_items(origins,destinations,outbound_date_strs,return_date_strs)
+#
+#     available_dict = get_airport_codes()
+#     dfs = []
+#     for i in iters:
+#         origin = i[0]
+#         destination = i[1]
+#         outbound_date_str = i[2]
+#         return_date_str = i[3]
+#         r = enter_single_search(origin,destination,outbound_date_str,return_date_str,available_codes=available_dict)
+#         soup = get_results_soup_request(r)
+#         df_temp = get_1way_df(soup,outbound_date_str,origin,destination,'outbound')
+#         add_timezones(df_temp,'origin','depart_time')
+#         add_timezones(df_temp,'destination','arrive_time')
+#         add_drivetimes(df_temp,'origin','depart_time','depart_drive_time')
+#
+#         dfs.append(df_temp)
+#     return dfs
 
 # end%%
 
@@ -403,85 +372,76 @@ def get_iter_items(origins,destinations,outbound_date_strs,return_date_strs):
 # end%%
 
 # %% Add timezones
-# def add_timezones(df,location_key,time_key):
-#     for city in df[location_key].unique():
-#         tz_code = get_timezone_code(city)
-#         df.loc[df[location_key] == city, time_key] = pd.Index(df[time_key].loc[df[location_key] == city].values).tz_localize(tz_code)
-#
-#     df[time_key] = pd.to_datetime(df[time_key])
 def app_timezone(row,location_key,time_key):
      tz_str = get_timezone_code(row[location_key])
-
      return pytz.timezone(tz_str).localize(row[time_key])
-
 # end%%
+
+
 
 # %% Testing single flight line by line
 available_dict = get_airport_codes()
+
 # d = get_driver()
 # enter_single_search(origin,destination,outbound_date_str,return_date_str,driver=d,available_codes=available_dict)
 r = enter_single_search('CLT','SLC','05/03/2018','05/07/2018',available_codes=available_dict)
-r.text
+
 soup = get_results_soup_request(r)
 soup
 df_outbound = get_1way_df(soup,'05/03/2018','CLT','SLC','outbound')
 df_return  = get_1way_df(soup,'05/03/2018','SLC','CLT','return')
-df
-df = pd.concat([df_outbound,df_return],axis=0).reset_index()
+# df = pd.concat([df_outbound,df_return],axis=0).reset_index()
+df = df_outbound
 df['depart_time'] = df.apply(lambda x: app_timezone(x,'origin','depart_time'),axis=1)
 df['arrive_time'] = df.apply(lambda x: app_timezone(x,'origin','arrive_time'),axis=1)
 # add_timezones(df,'origin','depart_time')
 # add_timezones(df,'destination','arrive_time')
 
 add_drivetimes(df,'origin','depart_time','depart_drive_time')
-get_sort_agony(df)
+print(get_sort_agony(df))
 # end%%
-# %%
-# enter_single_search('ATL','SLC','05/03/2018','05/07/2018',driver=d,available_codes=available_dict)
-df = do_all_single_flight('CLT','SLC','05/03/2018','05/07/2018')
+# # %%
+# # enter_single_search('ATL','SLC','05/03/2018','05/07/2018',driver=d,available_codes=available_dict)
+# df = do_all_single_flight('CLT','SLC','05/03/2018','05/07/2018')
+# # df
+# dfs = do_all_multiple_flight(['ATL','CLT'],['SLC'],['05/03/2018','05/04/2018'],['05/07/2018'])
+# # iters = get_iter_items(['ATL','CLT'],['SLC'],['05/03/2018'],['05/07/2018'])
+# df = pd.concat(dfs,axis=0).reset_index()
+# # iters[0][0]
+# df = get_sort_agony(df)
 # df
-dfs = do_all_multiple_flight(['ATL','CLT'],['SLC'],['05/03/2018','05/04/2018'],['05/07/2018'])
-# iters = get_iter_items(['ATL','CLT'],['SLC'],['05/03/2018'],['05/07/2018'])
-df = pd.concat(dfs,axis=0).reset_index()
-# iters[0][0]
-df = get_sort_agony(df)
-df
-# end%%
+# # end%%
+#
+# # %% Plot some shit
+# fig, ax = plt.subplots(figsize=(20,12))
+#
+# starts = dates.date2num(df['depart_time'].tolist()) - 4./24
+# stops = dates.date2num(df['arrive_time_act'].tolist()) - 4./24
+#
+# drive_starts = dates.date2num(df['depart_drive_time_stamp'].tolist()) - 4./24
+# drive_stops = dates.date2num((df['depart_drive_time_stamp'] + df['depart_drive_time']).tolist()) - 4./24
+#
+# ax.barh(range(len(df.index)),stops-starts,0.75,starts)
+# ax.barh(range(len(df.index)),drive_stops-drive_starts,0.75,drive_starts)
+# # rule = rrulewrapper(HOURLY, interval=2, byhour=1)
+# # loc = RRuleLocator(rule)
+# formatter = DateFormatter('%H')
+# ax.xaxis.set_major_locator(HourLocator(interval=2))
+# ax.xaxis.set_major_formatter(formatter)
+# labelsx = ax.get_xticklabels()
+# plt.setp(labelsx, rotation=30,ha='right')
+# ax.set_yticks(range(len(df.index)))
+# ax.yaxis.grid(False)
+# # ax.yaxis.set_visible(False)
+# df
+# for n, i in enumerate(df.index):
+#     ax.text((stops[n]- starts[n])/2 + starts[n] ,n,strfdelta(df['travel_time'].loc[i],'{H}:{M:02}'),va='center',ha='center',color='w')
+#     ax.text(starts[n]+ 10./60/24,n,df['origin'].loc[i],va='center',ha='left',color='w')
+#     ax.text(stops[n]- 10./60/24,n,df['destination'].loc[i],va='center',ha='right',color='w')
+#
+# ax.set_yticklabels(df['price'].apply(lambda x: '$%.0f'%(np.round(x))))
+# plt.show()
+# # end%%
 
-# %% Plot some shit
-fig, ax = plt.subplots(figsize=(20,12))
-
-starts = dates.date2num(df['depart_time'].tolist()) - 4./24
-stops = dates.date2num(df['arrive_time_act'].tolist()) - 4./24
-
-drive_starts = dates.date2num(df['depart_drive_time_stamp'].tolist()) - 4./24
-drive_stops = dates.date2num((df['depart_drive_time_stamp'] + df['depart_drive_time']).tolist()) - 4./24
-
-ax.barh(range(len(df.index)),stops-starts,0.75,starts)
-ax.barh(range(len(df.index)),drive_stops-drive_starts,0.75,drive_starts)
-# rule = rrulewrapper(HOURLY, interval=2, byhour=1)
-# loc = RRuleLocator(rule)
-formatter = DateFormatter('%H')
-ax.xaxis.set_major_locator(HourLocator(interval=2))
-ax.xaxis.set_major_formatter(formatter)
-labelsx = ax.get_xticklabels()
-plt.setp(labelsx, rotation=30,ha='right')
-ax.set_yticks(range(len(df.index)))
-ax.yaxis.grid(False)
-# ax.yaxis.set_visible(False)
-df
-for n, i in enumerate(df.index):
-    ax.text((stops[n]- starts[n])/2 + starts[n] ,n,strfdelta(df['travel_time'].loc[i],'{H}:{M:02}'),va='center',ha='center',color='w')
-    ax.text(starts[n]+ 10./60/24,n,df['origin'].loc[i],va='center',ha='left',color='w')
-    ax.text(stops[n]- 10./60/24,n,df['destination'].loc[i],va='center',ha='right',color='w')
-
-ax.set_yticklabels(df['price'].apply(lambda x: '$%.0f'%(np.round(x))))
-plt.show()
-# end%%
-
-# %% copy and Play
-df_new = df.copy()
-df_new.columns.values
-
-df_new
-# end%%
+if __name__ == '__main__':
+    main()
